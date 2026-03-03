4d6aa3d0d4e13f98977a2522f0111660d7221bc4
SamuelMedeirosA <samuel.medeiros014@gmail.com>
2026-03-03 19:31:06 -0300

    feat: study type void

---
+++ src/01-types-basics/typeVoid.ts
@@ -0,0 +1,11 @@
+const num1 = 10;
+const num2 = 32;
+let result: number;
+
+//example function without return (return type void)
+function multiply(): void {
+  result = num1 * num2;
+  console.log(result);
+}
+
+multiply();
